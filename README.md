dcb0828fb1525fc0d4c8ae16ba12d7be56341cc4
adewuyito <adewuyitimothy10@gmail.com>
2024-07-05 20:22:02 +0100

    ReadMe set up

--- README.md
+++ README.md
@@ -42,7 +42,37 @@ A Flutter application that simulates a simple shopping experience with product l
 ### Checkout Page
 ![Checkout Screen](assets/checkout_page.png)
 
+### Confirmation Page
+![Checkout Screen](assets/confirmation_page.png)
+
 
 ## Setup Instructions
 
 1. Clone the repository:
+git clone https://github.com/adewuyito/s_shoping_app.git
+
+2. Navigate to the project directory:
+cd s_shoping_app
+
+3. Install dependencies:
+flutter pub
+
+4. Run the app:
+flutter run
+
+## Download
+
+You can download the latest APK version of the app here: [Download APK](release/app-release.apk)
+
+## How to Use
+
+1. **Products Screen**: Browse through the list of available products. Tap the "Add to Cart" button to add an item to your checkout list.
+
+2. **Checkout Screen**: View items you've added to your cart. You can remove items by tapping the remove button next to each item. The total price is displayed at the bottom.
+
+3. **Order Confirmation**: On the Checkout screen, tap the "Place Order" button to navigate to the Order Successful screen.
+
+## Technologies Used
+
+- Flutter
+- Dart
--- lib/home_builder_view.dart
+++ lib/home_builder_view.dart
@@ -1,9 +1,9 @@
 import 'dart:developer';
 
 import 'package:flutter/material.dart';
+import 'package:s_shoping_app/common/extentions/text_extention.dart';
 import 'package:s_shoping_app/src/my_widgets.dart';
 import 'package:s_shoping_app/src/product_details.dart';
-import 'package:s_shoping_app/src/tests.dart';
 import 'package:s_shoping_app/utils/constants/sizes.dart';
 
 class HomeBuilder extends StatefulWidget {
@@ -144,28 +144,133 @@ class ProductView extends StatelessWidget {
   }
 }
 
-class CheckOutView extends StatelessWidget {
-  final List<ProductModel> productList;
+class CartPage extends StatefulWidget {
+  final List<ProductModel> productCartList;
+  final Function(int)? removeItem;
 
-  const CheckOutView({super.key, required this.productList});
+  const CartPage({
+    super.key,
+    required this.productCartList,
+    this.removeItem,
+  });
+
+  @override
+  State<CartPage> createState() => _CartPageState();
+}
+
+class _CartPageState extends State<CartPage> {
+  @override
+  Widget build(BuildContext context) {
+    double total = widget.productCartList
+        .fold(0, (sum, item) => sum + item.productPriceNo);
+    var cart = widget.productCartList;
+    return Scaffold(
+      floatingActionButton: cart.isNotEmpty
+          ? FloatingActionButton.extended(
+              elevation: 1,
+              onPressed: () {
+                Navigator.of(context).push(
+                  MaterialPageRoute(
+                    builder: ((context) => const OrderSuccessfulPage()),
+                  ),
+                );
+              },
+              isExtended: true,
+              icon: const Icon(Icons.shopping_cart_checkout_rounded),
+              label: "Check Out".toText(),
+            )
+          : null,
+      appBar: AppBar(title: const Text('Cart')),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Expanded(
+            child: ListView.builder(
+              itemCount: widget.productCartList.length,
+              itemBuilder: (context, index) {
+                final item = cart[index];
+                return ListTile(
+                  leading: SizedBox(
+                    width: 50,
+                    height: 50,
+                    child: Image.asset(
+                      item.productImage,
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                  title: Text(item.productName),
+                  subtitle: Text(item.productPrice),
+                  trailing: IconButton(
+                    icon: const Icon(
+                      Icons.remove_circle,
+                      color: Colors.redAccent,
+                    ),
+                    onPressed: () {
+                      setState(() {
+                        item.isFavourite = false;
+                      });
+                      widget.removeItem!(item.id);
+                    },
+                  ),
+                );
+              },
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Text(
+              'Total: \$${total.toStringAsFixed(2)}',
+              style: const TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class OrderSuccessfulPage extends StatelessWidget {
+  const OrderSuccessfulPage({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          for (int i = 0; i < 20; i++) {
-            log(productList[i].toString());
-          }
-        },
+      appBar: AppBar(
+        leading: IconButton(
+          iconSize: 35,
+          icon: Icon(Icons.navigate_before_rounded),
+          onPressed: () {
+            Navigator.popUntil(context, ModalRoute.withName('/'));
+          },
+        ),
       ),
       body: Center(
-        child: Container(
-          width: 200,
-          height: 250,
-          child: ProductItem(
-            productModel: productList[1],
-          ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Icon(
+              Icons.check_circle_outline_rounded,
+              color: Colors.green,
+              size: 100,
+            ),
+            const SizedBox(height: 20),
+            const Text(
+              'Order Placed Successfully!',
+              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+            ),
+            const SizedBox(height: 10),
+            const Text(
+              'Thank you for your purchase.',
+              style: TextStyle(fontSize: 16),
+            ),
+            const SizedBox(height: 30),
+            ElevatedButton(
+              child: const Text('Back to Home'),
+              onPressed: () {
+                Navigator.popUntil(context, ModalRoute.withName('/'));
+              },
+            ),
+          ],
         ),
       ),
     );
